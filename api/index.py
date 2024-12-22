5b339b02b5a3aa6d99250ef8a187cf307f6d20d3
Bernd Mayinger <bm.info.me@gmail.com>
2024-12-22 03:56:43 +0100

    Added form, added base64-support to the API

--- api/index.py
+++ api/index.py
@@ -1,15 +1,20 @@
 import time
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, render_template
+import base64
 import json
 import os
 import random
 from PIL import Image, ImageDraw, ImageFont
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.join('..', 'templates'))
 
-@app.route('/')
+@app.route('/test')
 def home():
     return 'Hello, World!'
+
+@app.route('/')
+def form():
+    return render_template('form.html')
 
 @app.route('/write-image', methods=['POST'])
 def write_image():
@@ -18,6 +23,7 @@ def write_image():
     heading = json_data.get('heading', '')
     subheading = json_data.get('subheading', '')
     text = json_data.get('text', '')
+    encoding = json_data.get('encoding', 'binary')  # Check for encoding parameter
 
     # Select a random image from the base-images folder
     image_files = [f for f in os.listdir('base-images') if f.endswith(('.jpg', '.jpeg', '.png'))]
@@ -33,16 +39,22 @@ def write_image():
     # Draw the text on the image
     draw.text((25, 300), heading, fill="white", font=ImageFont.load_default(67))
     draw.text((30, 500), subheading, fill="white", font=ImageFont.load_default(48))
-    draw.text((75, 750), text, fill="white", font=ImageFont.load_default(35))
+    draw.text((30, 750), text, fill="white", font=ImageFont.load_default(35))
 
     # Save the modified image to a BytesIO buffer
     from io import BytesIO
     buffer = BytesIO()
     image.save(buffer, 'PNG')
-    buffer.seek(0)
 
-    # Send the image as a binary response directly from the buffer
-    return send_file(buffer, mimetype='image/png')
+    if encoding == 'base64':
+        # Convert the image to base64 string with utf-8 encoding
+        buffer.seek(0)
+        base64_image = base64.b64encode(buffer.read()).decode('utf-8')
+        return base64_image
+    else:
+        # Send the image as a binary response directly from the buffer
+        buffer.seek(0)
+        return send_file(buffer, mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(debug=True)
